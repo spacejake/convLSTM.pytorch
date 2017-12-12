ef14e60cf1d7b380f8865db0e65a45fed49fdd19
agedres <andriyserdega@gmail.com>
2017-12-12 12:40:11 +0900

    Cuda check for flip method. Corrected

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from torch.autograd import Variable
 def flip_var(var, axis):
     idx = [i for i in range(var.size(axis)-1, -1, -1)]
     idx = Variable(torch.LongTensor(idx))
-    if var.is_cuda():
+    if var.is_cuda:
         idx = idx.cuda()
     inverted_tensor = var.index_select(axis, idx)
     return inverted_tensor
